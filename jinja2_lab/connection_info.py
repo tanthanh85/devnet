23b39a248a98a604c0e2680495a5b551a85c7887
Andy Thanh <tanthanh2285@gmail.com>
2022-12-03 09:54:45 +0700

    add Vault secret to netmilo send config

--- jinja2_lab/connection_info.py
+++ jinja2_lab/connection_info.py
@@ -1,29 +1,49 @@
 
+
+import hvac
+from hvac.exceptions import InvalidPath
+
+VAULT_ADDRESS = 'https://192.168.50.27:8200'
+VAULT_TOKEN = 'hvs.gbTTZLMI0canwYJpsKtN8bLR'
+try:
+    client = hvac.Client(url=VAULT_ADDRESS,token=VAULT_TOKEN,verify=False)
+    if client.is_authenticated():
+        print('successfully authenticated')
+        R1 = client.secrets.kv.v2.read_secret_version(mount_point='kv',path='R1')
+        R2 = client.secrets.kv.v2.read_secret_version(mount_point='kv',path='R2')
+        
+    else:
+        print('authentication failed')
+
+
+except InvalidPath as e:
+    print(e)
+
 connection_info = {
 
     'R1': {
 
-      'device_type': 'cisco_ios',
+      'device_type': R1['data']['data']['device_type'],
 
-      'host': '192.168.50.61',
+      'host': R1['data']['data']['host'],
 
-      'port': 22,
+      'port': R1['data']['data']['port'],
 
-      'username': 'admin',
+      'username': R1['data']['data']['username'],
 
-      'password': 'cisco'
+      'password': R1['data']['data']['password']
     },
     'R2': {
 
-      'device_type': 'cisco_ios',
+      'device_type': R2['data']['data']['device_type'],
 
-      'host': '192.168.50.62',
+      'host': R2['data']['data']['host'],
 
-      'port': 22,
+      'port': R2['data']['data']['port'],
 
-      'username': 'admin',
+      'username': R2['data']['data']['username'],
 
-      'password': 'cisco'
+      'password': R2['data']['data']['password']
     }
 
 
--- jinja2_lab/netmiko_send_config.py
+++ jinja2_lab/netmiko_send_config.py
@@ -2,6 +2,7 @@ from connection_info import *
 from netmiko import ConnectHandler
 
 for device in connection_info:
+
     with ConnectHandler(**connection_info[device]) as conn:
         conn.send_config_from_file('{device}_bgp_config.txt'.format(device=device))
   
