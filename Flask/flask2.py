b87d45c6508ad78329690684b63499e351febee5
Andy Thanh <tanthanh2285@gmail.com>
2022-12-13 20:46:23 +0700

    fine tune Flask Restx

--- Flask/flask2.py
+++ Flask/flask2.py
@@ -5,12 +5,12 @@ from flask_restx import Resource, Api, reqparse, fields
 iosxe = IOSXE()
 app = Flask(__name__)
 
-api = Api(app,version='1.0', title='SIMPLE IOSXE FLASK APP', description="This is a simple IOSXE Flask app")
-ns = api.namespace('IOSXE Flask sample', description="Flask sample")
+api = Api(app,version='1.0', title='SIMPLE IOSXE FLASK RESTX PAGE', description="This is a simple IOSXE Flask Restx document")
+ns = api.namespace('IOSXE Flask-RESTX sample', description="Flask-RESTX sample")
 
 
 parser = api.parser()
-parser.add_argument("area_id", type=str, required=True, help="OSPF Area ID example: 0, 1, 2, ...", location="form")
+parser.add_argument("area_id", type=str, required=False, help="OSPF Area ID example: 0, 1, 2, ...", location="form")
 parser.add_argument("int_id", type=str, required=True, help="GigabitEthernet Interface ID: 1, 2, ...", location="form")
 
 
@@ -19,18 +19,22 @@ parser.add_argument("int_id", type=str, required=True, help="GigabitEthernet Int
 # def hello_world():
 #     return "hello world"
 
-@ns.route('/getospf')
+@ns.route('/getospf/<int_id>')
 @api.doc(response={404: 'not found'})
 @api.doc(response={200: 'successful'})
+@api.doc(params={'int_id': 'Retrieve OSPF configuration on a GigabitEthernet interface'})
 class QueryInterfaces(Resource):
-    def get(self):
-        uri = 'Cisco-IOS-XE-native:native/interface/GigabitEthernet=1/ip/Cisco-IOS-XE-ospf:router-ospf/ospf/process-id'
+    def get(self,int_id):
+        uri = f'Cisco-IOS-XE-native:native/interface/GigabitEthernet={int_id}/ip/Cisco-IOS-XE-ospf:router-ospf/ospf/process-id'
         response = iosxe.get_data(uri)
-#         return response.json()
+        return response.json()
 
 @ns.route('/int_to_ospf')
-@api.doc(responses={403: "failed to create"})
+@api.doc(responses={403: "Authentication failed"})
+@api.doc(responses={404: "resource not found"})
+@api.doc(responses={500: "Internal Server Error"})
 @api.doc(responses={201: "success"})
+@api.doc(responses={200: "success"})
 class InterfaceToOSPF(Resource):
     @api.doc(parser=parser)
     def patch(self):
