b3dff3b9e2f5071539f2686ef29f6f7cab0b4107
Andy Thanh <tanthanh2285@gmail.com>
2022-12-22 19:59:18 +0700

    get bgp instance dynamically and push the configuration accordingly

--- netconf/bgp_config.py
+++ netconf/bgp_config.py
@@ -1,6 +1,7 @@
 from ncclient import manager
 from connection_info import *
 from get_loopback_IP import *
+import xmltodict
 
 
 
@@ -9,7 +10,7 @@ config = '''
     <native xmlns="http://cisco.com/ns/yang/Cisco-IOS-XE-native">
         <router>
           <bgp xmlns="http://cisco.com/ns/yang/Cisco-IOS-XE-bgp">
-            <id>11111</id>
+            <id>{id}</id>
             <address-family>
               <no-vrf>
                 <ipv4>
@@ -30,21 +31,34 @@ config = '''
       </native>
     </config>
 '''
-def get_bgp_instance():
-    pass
 
+filter = '''
+<native xmlns="http://cisco.com/ns/yang/Cisco-IOS-XE-native">
+        <router>
+          <bgp xmlns="http://cisco.com/ns/yang/Cisco-IOS-XE-bgp">
+            <id/>
+          </bgp>
+        </router>
+</native>
+'''
 
-def bgp_config(config):
-
-  with manager.connect(**R1) as m:
+def get_bgp_instance(m): 
+    if m.connected: 
+        reply = m.get(('subtree', filter))
+        reply_dict = xmltodict.parse(reply.xml)
+        return reply_dict['rpc-reply']['data']['native']['router']['bgp']['id']
+    
+def bgp_config(config,m):
     if m.connected:
         m.edit_config(config, target='running')
     
-
 if __name__ == '__main__':
     with manager.connect(**R1) as m:
+        id = get_bgp_instance(m)
         list_interfaces = retrieve_list_of_loopback_interfaces(m)
         network, mask = get_ip(m,list_interfaces)
-        print(network + mask)
-    print('Now adding the loopback network to BGP')
-    bgp_config(config=config.format(network=network,mask=mask))
+        print('adding the loopback network to BGP now...')
+        if id:
+            bgp_config(config=config.format(network=network,mask=mask,id=id),m=m)
+        
+   
