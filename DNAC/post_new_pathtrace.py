e33f0381af0d7132d13d280907b849a4e4a0c5f0
Andy Thanh <tanthanh2285@gmail.com>
2022-12-25 21:48:25 +0700

    add new pathtrace for DNAC lab

---
+++ DNAC/post_new_pathtrace.py
@@ -0,0 +1,43 @@
+import requests
+from dnac_login import get_token
+# from prettytable import PrettyTable
+import json
+import time
+
+# table = PrettyTable()
+
+status_code, token = get_token()
+
+data = {
+    "destIP": "10.10.20.166", 
+    "sourceIP": "10.10.20.163"
+}
+
+def post_pathtrace(token):
+    headers = {
+        'Content-Type': 'application/json',
+        'x-auth-token': token
+    }
+    url = 'https://sandboxdnac2.cisco.com/dna/intent/api/v1/flow-analysis'
+
+    response = requests.post(url=url,headers=headers,data=json.dumps(data),verify=False)
+    # response = requests.post(url=url,headers=headers,json=data,verify=False)
+    return response.status_code, response.json()['response']['flowAnalysisId']
+
+def get_flow_details(token,flow_id):
+    headers = {
+        'Content-Type': 'application/json',
+        'x-auth-token': token
+    }
+    url = f'https://sandboxdnac2.cisco.com/dna/intent/api/v1/flow-analysis/{flow_id}'
+    response = requests.get(url=url,headers=headers,verify=False)
+    return response.json()['response']['request']['status']
+
+if __name__ == '__main__':
+    status_code, flow_id = post_pathtrace(token=token)
+    print(flow_id)
+    time.sleep(5)
+
+    print(get_flow_details(token=token,flow_id=flow_id))
+
+    
