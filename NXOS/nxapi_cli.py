a26ce33f06662e48a11ef1e7a316a37caa4cc11b
Andy Thanh <tanthanh2285@gmail.com>
2022-12-31 20:08:11 +0700

    add nxos nxapi cli lab

---
+++ NXOS/nxapi_cli.py
@@ -0,0 +1,25 @@
+import requests
+import json
+
+headers = {
+    'content-type': 'application/json'
+}
+
+
+command = 'show ip int bri'
+payload = {
+  "ins_api": {
+    "version": "1.0",
+    "type": "cli_show",
+    "chunk": "0",
+    "sid": "sid",
+    "input": command,
+    "output_format": "json"
+  }
+}
+
+url = 'http://192.168.50.63/ins'
+
+response = requests.post(url=url,headers=headers,auth=('admin','cisco'),data=json.dumps(payload))
+
+print(response.json())
